a39e7cba565cc56e5ef0c3e074a9e31477bc1a4a
saeed shaikh <saeed_i_shaikh@whirlpool.com>
2020-02-28 09:24:10 +0530

    added back button and image view changes.

--- app/src/main/java/com/jet2travel/employeedata/employeeProfileDetail/EmployeeDetailActivity.java
+++ app/src/main/java/com/jet2travel/employeedata/employeeProfileDetail/EmployeeDetailActivity.java
@@ -1,12 +1,21 @@
 package com.jet2travel.employeedata.employeeProfileDetail;
 
+import android.app.Activity;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.jet2travel.employeedata.R;
 import com.jet2travel.employeedata.models.Employee;
 import com.jet2travel.employeedata.utils.AppConstant;
@@ -17,6 +26,7 @@ public class EmployeeDetailActivity extends AppCompatActivity implements IEmploy
     private TextView txtEmployeeName;
     private TextView txtEmployeeSalary;
     private TextView txtEmployeeAge;
+    private Activity activity;
     private EmployeeDetailPresenter presenter;
 
     @Override
@@ -25,6 +35,7 @@ public class EmployeeDetailActivity extends AppCompatActivity implements IEmploy
         setContentView(R.layout.activity_employee_detail_profile);
         getSupportActionBar().setTitle(getString(R.string.employee_details));
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        activity = this;
 
         initUI();
         presenter = new EmployeeDetailPresenter(this);
@@ -68,11 +79,34 @@ public class EmployeeDetailActivity extends AppCompatActivity implements IEmploy
                 txtEmployeeName.setText(employee.getEmployeeName());
             }
             if (employee.getEmployeeSalary() != null) {
-                txtEmployeeSalary.setText(employee.getEmployeeSalary()+"$");
+                txtEmployeeSalary.setText(employee.getEmployeeSalary() + "$");
             }
             if (Integer.toString(employee.getEmployeeAge()) != null) {
                 txtEmployeeAge.setText(Integer.toString(employee.getEmployeeAge()));
             }
+            if (TextUtils.isEmpty(employee.getProfileImage())) {
+                showProfileImage(employee.getProfileImage());
+            }
+        }
+    }
+
+
+    private void showProfileImage(String profileImage) {
+        if (!TextUtils.isEmpty(profileImage)) {
+            Glide.with(activity)
+                    .load(profileImage)
+                    .listener(new RequestListener<Drawable>() {
+                        @Override
+                        public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                            return false;
+                        }
+
+                        @Override
+                        public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                            return false;
+                        }
+                    })
+                    .into(imgEmployeeProfile);
         }
     }
 
